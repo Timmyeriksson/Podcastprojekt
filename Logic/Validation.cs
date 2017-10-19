610b43bc0763da6a5d252426f379808e2d053af1
Timmyeriksson <timmyerikericsson@gmail.com>
2017-10-19 16:07:31 +0200

    validering och feed

---
+++ Logic/Validation.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Logic
+{
+    public class Validation
+    {
+        public static bool tfNotEmpty(TextBox field, string label)
+        {
+            if (field.Text == "")
+            {
+                MessageBox.Show(label + label + "is a required field. Please enter a value before continuing.");
+                field.Focus();
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+        public static bool checkValidURL(string url)
+        {
+            try
+            {
+                var xml = "";
+                using (var client = new System.Net.WebClient())
+                {
+                    client.Encoding = Encoding.UTF8;
+                    xml = client.DownloadString(url);
+                    return true;
+                }
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Please enter a valid URL.");
+                return false;
+            }
+        }
+
+        public static bool categoryBoxNotEmpty(ComboBox cb, ListBox list)
+        {
+            if (cb.Text == "")
+            {
+                MessageBox.Show("Please choose the new category");
+                cb.Focus();
+                return false;
+            }
+            else if (cb.SelectedItem == list.SelectedItem)
+            {
+                MessageBox.Show("Podcast already belongs to that category");
+                cb.Focus();
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+        public static bool intervalBoxNotEmpty(ComboBox cb, ListBox list)
+        {
+            if(cb.Text == "")
+            {
+                MessageBox.Show("Please choose an update interval.");
+                cb.Focus();
+                return false;
+            }
+            else if(cb.SelectedItem == list.SelectedItem)
+            {
+                MessageBox.Show("The chosen update interval is already selected for the podcast");
+                cb.Focus();
+                return false;
+            }
+            else
+            {
+                return true; 
+            }
+        }
+
+        public static bool listBoxNotEmpty(ListBox list)
+        {
+            if (list.Text == "")
+            {
+                MessageBox.Show("Please choose a podcast to delete from the podcast list");
+                list.Focus();
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+    }
+}
